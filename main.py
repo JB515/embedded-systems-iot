bee89920135336696a405cb113620e1a84bee330
JB515 <jpb515@gmail.com>
2017-01-30 10:11:06 +0000

    Code for reading i2c input data

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
-def add(a, b):
-	return a+b
+from machine import Pin, I2C
+import ads1x15
+import time
 
-print(add(3,4))
+i2c = I2C(scl=Pin(5), sda=Pin(4), freq=100000)
+print(i2c.scan())
+
+sensor = ads1x15.ADS1115(i2c, 0x48)
+
+while True:
+	time.sleep(0.5)
+	data = sensor.read(0)
+	data = float (data*12) / 65535.0
+	print(data)
+	
