709898d8b608c9de3731a9a336eafa0dfa6718fd
BaronKhan <bksonic@hotmail.co.uk>
2017-02-11 18:15:04 +0000

    Format time in testSubscribe.py and tidy up repo

--- testSubscribe.py
+++ testSubscribe.py
@@ -1,9 +1,9 @@
 import paho.mqtt.client as mqtt  #import the client
 import time
 
-############################################################
-####################### OVERVIEW ###########################
-############################################################
+##################################################################################
+################################## OVERVIEW ######################################
+##################################################################################
 
 '''
 http://www.conservationphysics.org/wetstuff/wetstuff01.php
@@ -11,10 +11,9 @@ drying at 2m/s = approx 80 min to dry
 dryingTime = 160/(gAirFlow)
 '''
 
-###########################################################
-######################## GLOBALS ##########################
-###########################################################
-
+#################################################################################
+################################### GLOBALS #####################################
+#################################################################################
 
 #gBrokerAddress = "192.168.0.10"
 gBrokerAddress = "iot.eclipse.org"
@@ -26,9 +25,9 @@ gTotalTime = -1        #timer in seconds
 gTime = -1             #timer progress
 gAirFlow = 0.0
 
-###########################################################
-##################### MQTT FUNCTIONS ######################
-###########################################################
+#################################################################################
+################################ MQTT FUNCTIONS #################################
+#################################################################################
 
 # Callback functions for mqtt
 
@@ -39,7 +38,7 @@ def on_connect(client, userdata, flags, rc):
 
 def on_log(client, userdata, level, buf):
     pass
-    #print("log: ",buf)
+    #print("log: ",buf, flush = True)
     
 
 def on_message(c, userdata, message):
@@ -54,23 +53,22 @@ def on_message(c, userdata, message):
         else:
             updateTimer()
 
-###########################################################
-##################### OTHER FUNCTIONS #####################
-###########################################################
+#################################################################################
+################################ OTHER FUNCTIONS ################################
+#################################################################################
 
 def startTimer():
     global gAirFlow, gTotalTime, gStarted, gTime
-    if gAirFlow >= 0.0001:
+    if gAirFlow >= 0.00001:
         #calculate new drying timer in seconds
         gTotalTime = int((9600.0/float(gAirFlow)))  
-        print ("Starting drying timer! Total time: "+
-            str(gTotalTime)+"s", flush = True)
+        print ("Starting timer! Total time: "+
+            timeStr(gTotalTime)+"s", flush = True)
         gStarted = True
         gPaused = False
-
     else:
         gTotalTime = -1
-        print ("Cannot start drying timer         ", flush = True)
+        print ("Cannot start timer                      ", flush = True)
         gStarted = False
     gTime = gTotalTime
 
@@ -78,16 +76,16 @@ def updateTimer():
     global gAirFlow, gPaused, gTime, gTotalTime
     if gAirFlow <= 0.00001:
         gPaused = True
-        print("Timer paused, waiting for air flow.       ", flush = True)
+        print("Timer paused, waiting for air flow.      ", flush = True)
     else:
         gPaused = False
         #calculate current progress so far
-        #then calculate what the new total time would be with new speed
-        #calculate gTime based on progress
         progress = float(gTime)/float(gTotalTime)
+        #then calculate what the new total time would be with new speed
         gTotalTime = int((9600.0/float(gAirFlow)))
+        #calculate gTime based on progress
         gTime = int(float(gTotalTime)*progress)
-        print("Updating time: "+str(gTime)+"         ", flush = True)
+        print("Updating time: "+timeStr(gTime)+"        ", flush = True)
 
 def timeTick():
     global gTime, gTotalTime, gStarted, gPaused
@@ -97,17 +95,22 @@ def timeTick():
             #Send alarm when finished - todo
             gStarted = False
             gTotalTime = gTime = -1
-            print("\nFinished drying clothes!      ", flush = True)
+            print("\nFinished drying clothes!           ", flush = True)
 
+def timeStr(t):
+    hours = int(t/3600)
+    minutes = int((t%3600)/60)
+    seconds = int((t%3600)%60)
+    return str(hours).zfill(2)+":"+str(minutes).zfill(2)+":"+str(seconds).zfill(2)
 
-###########################################################
-########################## MAIN ###########################
-###########################################################
+#################################################################################
+##################################### MAIN ######################################
+#################################################################################
 
 print("starting client", flush = True)
-c = mqtt.Client("P1", protocol=mqtt.MQTTv31)       #create new instance
-c.on_connect = on_connect   #attach function to callback
-c.on_message = on_message   #attach function to callback
+c = mqtt.Client("P1", protocol=mqtt.MQTTv31)        #create new instance
+c.on_connect = on_connect                           #attach callback functions
+c.on_message = on_message
 c.on_log = on_log
 time.sleep(1)
 print("attempting to connect to broker", flush = True)
@@ -115,13 +118,14 @@ c.connect(gBrokerAddress)   #connect to broker
 print("starting loop")
 c.loop_start()              #start the loop
 c.subscribe(gTopic)
-print("checking for messages", flush = True)
 time.sleep(5)
+print("checking for messages", flush = True)
 while True:
     time.sleep(1)
     c.subscribe(gTopic)
-    if gStarted:
-        print("Time to dry: "+str(gTime)+"s         ", end="\r", flush = True)
+    if gStarted and not gPaused:
+        print("Time to dry: "+timeStr(gTime)+"s         ", end="\r", flush = True)
         timeTick()
+
 c.disconnect()
 c.loop_stop()
